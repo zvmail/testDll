90c4d07d142b2fdc7735c410d16fc68902b62fc9
vincent.zhang <zvmail@21cn.com>
2017-04-27 17:00:21 +0800

    init

---
+++ src/testDll/Test.java
@@ -0,0 +1,29 @@
+package testDll;
+
+import net.sf.jni4net.Bridge;
+
+import jcarddll.JcardInterface;
+
+public class Test {
+	
+	public static void main(String[] args) throws Exception {
+
+		Bridge.init();
+        Bridge.LoadAndRegisterAssemblyFrom(new java.io.File("dll/JcardDLL.j4n.dll"));
+		
+        JcardInterface jcard = new JcardInterface();
+        
+        jcard.setenqmembercode("ddd");
+        String ret = jcard.enquire_member();
+        
+		System.out.println(ret);
+		System.out.println(jcard.getenqmemberstatus());
+		System.out.println(jcard.getenqbpexpired());
+		System.out.println(jcard.getenqbpexpirydate());
+		System.out.println(jcard.getenqbplastday());
+		System.out.println(jcard.getenqbpused());
+		System.out.println(jcard.getenqmessage());
+				
+		System.exit(0);
+	}
+}
